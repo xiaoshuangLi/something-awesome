d9983349c71e85308c3d0efcf2ee91c6d6bbaf6b
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-05-04 17:53:50 +0800

    delete gulp

--- gulp/frontend/gulp/tasks/watchify.js
+++
@@ -1,7 +0,0 @@
-
-var gulp = require('gulp');
-var browserifyTask = require('./browserify');
-
-gulp.task(global.gulpOptions.prefix + 'watchify', function (callback) {
-  browserifyTask(callback, true);
-});
--- gulp/gulp/tasks/clean.js
+++
@@ -1,8 +0,0 @@
-var config = require('../config').clean;
-
-var gulp = require('gulp');
-var del = require('del');
-
-gulp.task('clean', [global.gulpOptions.prefix + 'clean'], function () {
-  return del(config.files);
-});
