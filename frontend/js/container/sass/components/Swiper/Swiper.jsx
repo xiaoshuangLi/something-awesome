eb361fc5335ae79b9974335a615cae2d65ca57c0
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-05-08 00:08:44 +0800

    add speech

--- frontend/js/container/sass/components/Swiper/Swiper.jsx
+++ frontend/js/container/sass/components/Swiper/Swiper.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { logSpeech } from 'js/common';
 
 import Ed from './ed.jpeg';
 import Cave from './cave.jpeg';
@@ -23,11 +24,32 @@ class Swiper extends Component {
     ];
 
     const obj = list.reduce((a, b,i) => {
-      a[`data-img-${i}`] = b.img
+      // a[`data-img-${i}`] = b.img
       a[`data-title-${i}`] = b.title
 
       return a;
     }, {})
+
+    const speech = [
+      '走马灯/swiper页面',
+      { type: 'desc', txt: 'box-shadow + background' },
+      '继续说明下这个页面，这是一个走马灯，也就是大家熟知的swiper组件。左上角是图片的标题，右下角是走马灯滚动的位置，这些都是会随着图片的滚动而变化。',
+      { type: 'desc', txt: '需不需要一些时间欣赏一下，过会，我又要问一个比较尴尬的问题。' },
+      '好吧，姐妹们用心回答我。',
+      '这个页面最少需要几个标签来实现？',
+      { type: 'desc', txt: '嘿嘿嘿。。。' },
+      'line',
+      '答案还是一个。实现的思路和前两个示例还是一样不过需要用到其他两个css的属性，content, background. ',
+      '首先一个标签我们有三个元素可以用，便签本身（以下简称:div），before，after。这里div我们用做显示图片，before显示图片标题，after显示滚动位置。',
+      '在看sass代码之前，我们要看看这个标签，为了实现效果，要对标签做一下处理。',
+      '下面介绍一下background和content这两个属性',
+      'line',
+      '这里用到的sass功能我就不一一赘述，但是绝对只是冰山一角，希望大家可以前去挖掘。',
+      '推荐一个sass的方法库math-sass https://www.npmjs.com/package/mathsass',
+      '里面实现了很多基本的数学计算，比如三角函数，指数计算等等。',
+    ];
+
+    logSpeech(speech);
 
     this.setState({obj, list})
 
--- frontend/js/container/sass/components/Wave/Wave.jsx
+++ frontend/js/container/sass/components/Wave/Wave.jsx
@@ -1,6 +1,39 @@
 import React, { Component } from 'react';
+import { logSpeech } from 'js/common';
 
 class Wave extends Component {
+
+	componentDidMount() {
+		const list = [
+			'Loading页面',
+			{ type: 'desc', txt: 'box-shadow的使用' },
+			'这个一个简单的Loading页面，正中一句话来提示用户，下方有一个类似波纹的动画效果。看不清的话我把地址发到群里面，或者我把颜色挑明显一点。',
+			{ type: 'desc', txt: '问一下大家是不是明白的内容了。' },
+			'让我们思考一个问题，这个页面最少只需要用到几个标签来实现。',
+			{ type: 'desc', txt: '等待大家回答完毕，或者听完歌了。' },
+			'line',
+			'答案嘛，说出来你们可能不信，只用到了1个标签。',
+			{ type: 'desc', txt: '唉，早知道我就应该开个盘下个注，至少亏了几个肾。' },
+			'首先说明一点，一个标签，除了input这个输入的表单之外，都可以个拥有before和after伪类，所以一般来说一个标签可以看作三个标签来使用。',
+			'在这个示例里面，中间这句话显而易见就是beofre了。难点在下面，这个波纹应该怎么做。有没有同学有些可能的想法，除了最福利和i云保的，乖乖坐好不要剧透。',
+			'line',
+			'答案是box-shadow，这个我们平时经常用到的属性。',
+			'在我们的印象里面，box-shadow应该只是一个阴影的属性。怎么可以这样，我不糊（服）啊？',
+			{ type: 'desc', txt: '查看代码，了解box-shadow' },
+			'正如你所见，box-shadow的确只阴影的属性，但是不是一个是多个。多个shadow的属性可以同时作用在一个元素上，来形成这个波纹效果。',
+			'可能听到这里大概认为这个示例所说的重点就是box-shadow, 如果真事这样想的,too young啊，兄弟们.',
+			'box-shadow只是一个属性，一个静态的属性，可是我们的结果是一个动态效果，这个应该怎么做。难道要手动计算所有的变化结果写到keyframes里面。',
+			{ type: 'desc', txt: '语气变化。' },
+			'答案是的，不过不是我们计算，是sass计算。让我们打开代码看一下。',
+			{ type: 'desc', txt: '查看代码，了解sass' },
+			{ type: 'desc', txt: '理科男偷笑。' },
+			'这里sass所体现的功能有 function， for循环， array数组储存变量， 数字与百分比的转换。',
+			'同志们有什么疑问吗，没有的话我们就看下一个示例了。',
+			{ type: 'desc', txt: '大家还有什么问题吗。' },
+		];
+
+		logSpeech(list);
+	}
 
   render() {
     return (
