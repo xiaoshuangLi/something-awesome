0c3d0115d3c3fe3b02da62e7a8541dd496aa000a
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-05-14 17:52:29 +0800

    add speech

--- frontend/js/container/sass/components/Swiper/Swiper.jsx
+++ frontend/js/container/sass/components/Swiper/Swiper.jsx
@@ -35,7 +35,7 @@ class Swiper extends Component {
       { type: 'desc', txt: 'box-shadow + background' },
       '继续说明下这个页面，这是一个走马灯，也就是大家熟知的swiper组件。左上角是图片的标题，右下角是走马灯滚动的位置，这些都是会随着图片的滚动而变化。',
       { type: 'desc', txt: '需不需要一些时间欣赏一下，过会，我又要问一个比较尴尬的问题。' },
-      '好吧，姐妹们用心回答我。',
+      '好吧，还是一样的问题。',
       '这个页面最少需要几个标签来实现？',
       { type: 'desc', txt: '嘿嘿嘿。。。' },
       'line',
@@ -47,6 +47,7 @@ class Swiper extends Component {
       '这里用到的sass功能我就不一一赘述，但是绝对只是冰山一角，希望大家可以前去挖掘。',
       '推荐一个sass的方法库math-sass https://www.npmjs.com/package/mathsass',
       '里面实现了很多基本的数学计算，比如三角函数，指数计算等等。',
+      '没有问题的话，sass使用的示例就说完了，开始svg滤镜的示例。',
     ];
 
     logSpeech(speech);
