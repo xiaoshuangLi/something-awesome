1c40c0f3e60bf4386892d3a21c7bc86ceb43c09b
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-05-14 17:52:29 +0800

    add speech

--- frontend/js/container/svg/pages/Menu/Menu.jsx
+++ frontend/js/container/svg/pages/Menu/Menu.jsx
@@ -10,7 +10,7 @@ import DarkSouls2 from 'img/dark_souls_2.jpg';
 import FightClub from 'img/fight_club.jpg';
 import FightClub2 from 'img/fight_club_2.jpeg';
 
-import { getEles } from 'js/common';
+import { getEles, logSpeech } from 'js/common';
 
 const data = [
   { name: 'Ed Sheeran', img: [ Ed, Ed2 ] ,content: 'Ed Sheeran, 又称黄老板，一位英国歌手。' },
@@ -92,6 +92,25 @@ class Menu extends Component {
 
     const Line = _createLine(list);
     Line.play();
+
+
+    const speech = [
+      '水滴效果菜单',
+      { type: 'desc', txt: 'svg 滤镜 + gsap实现流畅动画。' },
+      'svg个人理解可以分为三个部分来学习，画图，动画，滤镜。这次主要介绍滤镜这方面的功能。',
+      '首先介绍一下示例，这是一个简单的菜单，点击每个菜单，会显示不同的内容。',
+      '这个示例的麻烦的地方有两个，一个动画的实现, 另一个就是这种黏糊糊的效果, 也就是本示例的重点svg滤镜。',
+      '让我们看下代码。首先看一下svg的定义。',
+      'line',
+      '这样就实现了这种黏糊糊的效果。以上只是svg滤镜里面的三种，还有其他十几种滤镜可以使用，以后有机会再向大家一一介绍。',
+      '很遗憾的是，这种效果在移动端并不理想。估计等个四五年，我就可以抛弃svg和html，直接用canvas实现编写页面。',
+      '大家还有什么疑问吗？',
+      'line',
+      '没有其他的疑问话，以上就是本次分享的全部内容。不能说给大家在实际开发里带来多少有用的干货，但是希望能给大家在编写页面的时候带来一写不一样的思路，写出一些自己会很享受的页面',
+      '如果耽误了大家吃饭的时间，我表示非常的开心，靠，好饿。',
+    ];
+
+    logSpeech(speech);
   }
 
   componentDidUpdate() {
