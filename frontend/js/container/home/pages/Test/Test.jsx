efcc3f9a7cab382b3160e19b47d2a036807ac7cb
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-08-03 15:31:50 +0800

    add transform-class-properties & eslint

--- frontend/js/container/home/pages/Test/Test.jsx
+++ frontend/js/container/home/pages/Test/Test.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
-
+  test = () => {
+    console.log(this);
+  }
   render() {
+    this.test();
+
     return (
       <div className="page-home-test-render">
         test
